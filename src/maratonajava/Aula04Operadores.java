22caeea02b8d95fcdbedd5cd6f8aa4c0a5cfe3c3
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-06-30 17:21:45 -0300

    Adicionado operadores

---
+++ src/maratonajava/Aula04Operadores.java
@@ -0,0 +1,20 @@
+package maratonajava;
+
+public class Aula04Operadores {
+
+	public static void main(String[] args) {
+		int n1 = 10;
+		int n2 = 20;
+		double resultado = n1 / n2;
+		
+		System.out.println(n1 + n2);
+		System.out.println(n1 - n2);
+		System.out.println(n1 * n2);
+		System.out.println(n1 / n2);
+		System.out.println(resultado);
+		System.out.println("Valor: " + n1 + n2);
+		System.out.println("Valor: " + (n1 + n2));
+
+	}
+
+}
