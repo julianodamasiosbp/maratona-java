d548c3e2b9d32286f73945a415a642f6df7ecea2
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-06-30 16:58:57 -0300

    Adicionado exercicio tipos primitivos

--- src/maratonajava/Aula02TiposPrimitivos.java
+++ src/maratonajava/Aula02TiposPrimitivos.java
@@ -4,9 +4,9 @@ public class Aula02TiposPrimitivos {
 
 	public static void main(String[] args) {
 		int idade = 10;
-		long numeroGrande = 4_000_000;
-		double salarioDouble = 2000;
-		float salarioFloat = 2500;
+		long numeroGrande = 4_000_000L;
+		double salarioDouble = 2000.0D;
+		float salarioFloat = 2500.0F;
 		byte idadeByte = 127;
 		// byte idadeByte = 128; apresenta erro, já que o tipo Byte vai até 127
 		byte valorByteNegativo = -128;
@@ -15,10 +15,11 @@ public class Aula02TiposPrimitivos {
 		boolean falso = false;
 		char genero = 'M';
 		char asciitable = 60;
+		int idadeCasting = (int) 100000000000L;
 		
 		System.out.println("A sua idade é: " + idade + "anos");
 		System.out.println("Valor ASCII: " + asciitable);
-		
+		System.out.println(idadeCasting);
 
 	}
 
---
+++ src/maratonajava/Aula03TiposPrimitivosExercicio.java
@@ -0,0 +1,15 @@
+package maratonajava;
+
+public class Aula03TiposPrimitivosExercicio {
+	public static void main(String[] args) {
+		
+		String nome = "João Valentão";
+		double salario = 1420;
+		String endereco = "Rua das laranjeiras, 402";
+		String dataRecebimentoSalario = "10/10/2025";
+		
+		System.out.printf("Eu %s , morando no endereço %s, "
+				+ "confirmo que recebi o salário de %.2f na data %s", nome, endereco, salario, dataRecebimentoSalario);
+	}
+
+}
