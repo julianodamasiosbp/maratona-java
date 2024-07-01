03d2f13f4d283a8f6020b7099b1001232bcceab8
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-01 16:52:20 -0300

    Operadores logicos AND

--- src/maratonajava/Aula04Operadores.java
+++ src/maratonajava/Aula04Operadores.java
@@ -10,11 +10,42 @@ public class Aula04Operadores {
 		System.out.println(n1 + n2);
 		System.out.println(n1 - n2);
 		System.out.println(n1 * n2);
-		System.out.println(n1 / n2);
+		System.out.println(n1 / (double) n2);
 		System.out.println(resultado);
 		System.out.println("Valor: " + n1 + n2);
 		System.out.println("Valor: " + (n1 + n2));
 
+		// Resto da divisão %
+
+		int resto = 21 % 7;
+		int resto2 = 20 % 3;
+		System.out.println("Resto da divisão: " + resto);
+		System.out.println("Resto da divisão: " + resto2);
+
+		// < (menor que) > (maior que)
+		// <= (menor ou igual que) >= (maior ou igual que)
+		// == (igual) != (diferente)
+
+		boolean isDezMaiorQueVinte = 10 > 20;
+		boolean isDezMenorQueVinte = 10 < 20;
+		boolean isDezIgualVinte = 10 == 20;
+		System.out.println(isDezMaiorQueVinte);
+		System.out.println(isDezMenorQueVinte);
+		System.out.println(isDezIgualVinte);
+
+		// Operadores Lógicos
+		// && AND
+		// || OR
+
+		int idade = 29;
+		double salario = 3500F;
+		boolean isDentroDaLeiMaiorQueTrinta = idade > 30
+				&& salario >= 4612;
+		boolean isDentroDaLeiMenorQueTrinta = idade < 30
+				&& salario >= 3381;
+
+		System.out.println("isDentroDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
+		System.out.println("isDentroDaLeiMenorQueTrinta: " + isDentroDaLeiMenorQueTrinta);
 	}
 
 }
