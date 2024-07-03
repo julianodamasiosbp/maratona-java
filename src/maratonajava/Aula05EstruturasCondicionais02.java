23d4252b862dd9183c8b4375856bda138a9dd2e1
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-03 17:41:21 -0300

    Adicionado estrutura condicionais parte 2

--- src/maratonajava/Aula05EstruturasCondicionais02.java
+++ src/maratonajava/Aula05EstruturasCondicionais02.java
@@ -6,13 +6,16 @@ public class Aula05EstruturasCondicionais02 {
         // idade >= 15 && idade < 18 categoria juvenil
         // idade >= 18 categoria adulto
         int idade = 17;
+        String categoria;
         if(idade < 15) {
-            System.out.println("Categoria Infantil");
+            categoria = "Categoria Infantil";
         } else if(idade >= 15 && idade < 18) {
-            System.out.println("Categoria juvenil");
+            categoria = "Categoria Juvenil";
         } else {
-            System.out.println("Categoria Adulto");
+            categoria = "Categoria Adulto";
         }
+
+        System.out.println(categoria);
 
     }
 }
---
+++ src/maratonajava/Aula05EstruturasCondicionais03.java
@@ -0,0 +1,12 @@
+package maratonajava;
+
+public class Aula05EstruturasCondicionais03 {
+    public static void main(String[] args) {
+        // Doacao se salário maior que 5000
+        double salario = 3000;
+        String mensagemDoacao = "Eu vou doar 500 para o DevDojo";
+        String mensagemNaoDoar = "Ainda não tenho condicoes, mas vou ter";
+        String resultado = salario > 5000 ? mensagemDoacao : mensagemNaoDoar;
+        System.out.println(resultado);
+    }
+}
