44127476e6e81727ac43d36a16dfd6bfdde288f6
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-05 14:37:22 -0300

    Adicionado Estrutura de Repeticao

---
+++ src/maratonajava/Aula06EstruturasDeRepeticao01.java
@@ -0,0 +1,19 @@
+package src.maratonajava;
+
+public class Aula06EstruturasDeRepeticao01 {
+    public static void main(String[] args) {
+        // While, Do while, For
+        int count = 0;
+        while(count < 10){
+            System.out.println(++count);
+        }
+        count = 0;
+        do {
+            System.out.println("Dentro do do-while "+ ++count);
+        } while (count < 10);
+        
+        for(int i = 1 ; i <= 10 ; i++){
+            System.out.println("For " + i);
+        }
+    }
+}
