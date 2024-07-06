a3add3fed037c5af8f3f4477e700fa9361b7882e
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-06 17:18:02 -0300

    Adicionado aula 06 parte 03 continue

---
+++ src/maratonajava/Aula06EstruturasDeRepeticao03.java
@@ -0,0 +1,18 @@
+package maratonajava;
+
+public class Aula06EstruturasDeRepeticao03 {
+
+	public static void main(String[] args) {
+		double valorTotalDoCarro = 30000;
+		
+		for(int parcela = (int) valorTotalDoCarro; parcela >= 1; parcela--) {
+			double valorParcela = valorTotalDoCarro / parcela;
+			if(valorParcela < 1000) {
+				continue;
+			}
+			System.out.printf("Parcela %d R$ %.2f \n", parcela, valorParcela);
+		}
+
+	}
+
+}
