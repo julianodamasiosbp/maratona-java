497e00cf9271ead846c80da1887a401bf93fb26a
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-18 22:13:24 -0300

    Adicionado aula sobre referencia this\

--- src/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
+++ src/maratonajava/javacore/Bintroducaometodos/dominio/ImpressoraEstudante.java
@@ -2,7 +2,10 @@ package src.maratonajava.javacore.Bintroducaometodos.dominio;
 
 public class ImpressoraEstudante {
     public void imprime(Estudante estudante) {
-        System.out.println("------------------");
+        System.out.println("------------------ Dentro do ImpressoraEstudante ------------------");
+        estudante.nome = "AAA";
+        estudante.idade = 2;
+        estudante.sexo = 'S';
         System.out.println(estudante.nome);
         System.out.println(estudante.idade);
         System.out.println(estudante.sexo);
--- src/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
+++ src/maratonajava/javacore/Bintroducaometodos/test/EstudanteTest01.java
@@ -13,12 +13,19 @@ public class EstudanteTest01 {
         est.idade = 22;
         est.sexo = 'M';
 
+        System.out.println("------------------ Dentro do EstudanteTest01 ------------------");
+        System.out.println(est.nome);
+        System.out.println(est.idade);
+        System.out.println(est.sexo);
+        System.out.println("------------------");
+
         est2.nome = "Maria";
         est2.idade = 21;
         est2.sexo = 'F';
 
         impressora.imprime(est);
 
+        System.out.println("------------------ Dentro do EstudanteTest01 ------------------");
         System.out.println(est.nome);
         System.out.println(est.idade);
         System.out.println(est.sexo);
@@ -26,5 +33,8 @@ public class EstudanteTest01 {
         System.out.println(est2.nome);
         System.out.println(est2.idade);
         System.out.println(est2.sexo);
+
+        est.imprime();
+        est2.imprime();
     }
 }
