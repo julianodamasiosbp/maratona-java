5179a06ba8aa39e055da412b1cbc81a45d269ba6
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-07-24 09:57:31 -0300

    Adicionado melhorias no exercicio

--- src/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
+++ src/maratonajava/javacore/Bintroducaometodos/dominio/Funcionario.java
@@ -9,23 +9,25 @@ public class Funcionario {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         for(double salario : this.salarios) {
-            System.out.println("Salário: " + salario);
+            System.out.print(salario + " ");
         }
+        System.out.println();
     }
 
     public void mediaSalario() {
-        double somaSalario = 0;
+        double media = 0;
         for(double salario : this.salarios) {
-            somaSalario += salario;
+            media += salario;
         }
-        System.out.println("A média dos salários é: " + somaSalario / this.salarios.length);
+        System.out.println("A média dos salários é: " + media / this.salarios.length);
     }
 
     public void adicionarSalario(double salario) {
+        if(this.salarios[2] > 0) {
+            System.out.println("Não é possível adicionar mais salário");
+            return;
+        }
         for(int i = 0; i < this.salarios.length; i++) {
-            if(this.salarios[2] > 0) {
-                System.out.println("Não é possível adicionar mais salário");
-            }
             if(this.salarios[i] > 0) {
                 continue;
             }
