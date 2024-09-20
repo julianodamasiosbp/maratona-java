ef1c6c1b095708c529ad7e974337db75d0037e91
Juliano Damasio <julianodamasiosbp@gmail.com>
2024-09-20 20:27:43 -0300

    interfaces

---
+++ src/maratonajava/javacore/Lclassesabstratas/dominio/Funcionario.java
@@ -0,0 +1,20 @@
+package src.maratonajava.javacore.Lclassesabstratas.dominio;
+
+public abstract class Funcionario extends Pessoa {
+
+    protected String nome;
+    protected double salario;
+
+    public Funcionario(String nome, double salario) {
+        this.nome = nome;
+        this.salario = salario;
+        calcularBonus();
+    }
+
+    @Override
+    public void imprime() {
+        System.out.println("Imprimindo...");
+    }
+
+    public abstract void calcularBonus();
+}
